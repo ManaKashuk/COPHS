1e21ffbf9c5220cb02f10bc36b842c38ceddc95c
Mana Kashuk <140280356+ManaKashuk@users.noreply.github.com>
2026-03-29 18:39:33 -0500

    Update suppository_calculator.py

--- suppository_calculator.py
+++ suppository_calculator.py
@@ -251,7 +251,7 @@ st.markdown(
     """
     <hr style="margin-top:2rem; margin-bottom:0.5rem;">
     <div style="text-align:center; font-size:0.9rem; color:gray;">
-        © 2025 College of Pharmacy and Health Sciences<br>
+        © 2026 College of Pharmacy and Health Sciences<br>
         For citation (APA): College of Pharmacy and Health Sciences. (2025). 
         <i>COPHS Suppository Base Calculator</i> [Computer software]. 
         <a href="https://cophs-calculator.streamlit.app/" target="_blank">
